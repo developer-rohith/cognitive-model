4a8903a46e9737e2bc342584cb05724af11f5894
developer-rohith <rohith18srisai@gmail.com>
2023-11-12 23:33:47 -0500

    first commit

---
+++ src/Sample.java
@@ -0,0 +1,30 @@
+
+import java.io.FileNotFoundException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SAI
+ */
+public class Sample {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        // TODO code application logic here
+//       menuform m =  new menuform();
+//        m.setVisible(true);
+        
+        login l=new login();
+        l.setVisible(true);
+        
+    }
+    
+}
